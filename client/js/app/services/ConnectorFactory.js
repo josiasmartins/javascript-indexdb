af73193e3c72d1d9a0d926c97c4cd79a3adedfc8
Caitano <josiasmartinscaitano@gmail.com>
2022-11-13 16:44:53 -0300

    feat: criando store

--- client/js/app/services/ConnectorFactory.js
+++ client/js/app/services/ConnectorFactory.js
@@ -18,17 +18,32 @@ class ConnectionFactory {
 
             // onupgradeneeded: usado para criar as store
             openRequest.onupgradeneeded = e => {
-                console.log9(e);
-
+                console.log(e);
+                ConnectionFactory._createStores(e.target.result);
             }
-
+            
             openRequest.onsuccess = e => {
+                console.log(`onsuccess: `+e);
+                
+                resolve(e.target.result);
 
             };
-
+            
             openRequest.onerror = e => {
+                console.log(`onsuccess: `+e, e.target.error);
 
+                reject(e.target.error.name);
             }
+        })
+    }
+    
+    static _createStores(connection) {
+        
+        stores.forEach(store => {
+    
+            // se existe um object store, delete e cria uma nova
+            if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+            connection.createObjectStore(store, { autoIncrement: true })
         })
     }
 }
