10edb0f8b548b68ec731ab901751cc3425c6bc86
Caitano <josiasmartinscaitano@gmail.com>
2022-11-20 20:43:46 -0300

    feat: implementado o polyfill

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -20,27 +20,5 @@ class HttpService {
             body: JSON.stringify(dado)
         })
         .then(res => this._handleErrors(res));
-
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('Content-type', 'application/json');
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-
-            xhr.send(JSON.stringify(dado));
-        });
     }
 }
